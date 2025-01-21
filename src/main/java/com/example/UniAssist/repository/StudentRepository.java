641bee6815f547fc137dfb96339e475863e7f62d
NKtym <Pasha04072004@yandex.ru>
2025-01-21 17:33:13 +0700

    Add Teacher repository

--- src/main/java/com/example/UniAssist/repository/StudentRepository.java
+++ src/main/java/com/example/UniAssist/repository/StudentRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -13,4 +14,7 @@ public interface StudentRepository extends JpaRepository<Student, UUID> {
 
     @Query("SELECT s.groupId FROM Student s WHERE s.id = :studentId")
     UUID findGroupIdByStudentId(@Param("studentId") UUID studentId);
+
+    @Query("SELECT s FROM Student s WHERE s.login = :login AND s.password = :password")
+    Optional<Student> findByLoginAndPassword(@Param("login") String login, @Param("password") String password);
 }
