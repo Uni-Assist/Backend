386de26d3662965e9a73e58af1f3495c6f152a0c
NKtym <Pasha04072004@yandex.ru>
2025-01-29 23:44:08 +0700

    DTO in the controller

--- src/main/java/com/example/UniAssist/controller/AuthController.java
+++ src/main/java/com/example/UniAssist/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.UniAssist.controller;
 
+import com.example.UniAssist.model.dto.StudentDTO;
+import com.example.UniAssist.model.dto.TeacherDTO;
 import com.example.UniAssist.service.AuthService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,22 +23,24 @@ public class AuthController {
     }
 
     @PostMapping("/login/teacher")
-    public ResponseEntity<Map<String, Object>> teacherLogin(
+    public ResponseEntity<TeacherDTO> teacherLogin(
             @RequestBody Map<String, String> credentials) {
         String login = credentials.get("login");
         String password = credentials.get("password");
 
         Map<String, Object> response = authService.authenticateTeacher(login, password);
-        return ResponseEntity.ok(response);
+        TeacherDTO teacherDTO = new TeacherDTO((UUID) response.get("teacher_id"), (String) response.get("role"));
+        return ResponseEntity.ok(teacherDTO);
     }
 
     @PostMapping("/login/student")
-    public ResponseEntity<Map<String, Object>> studentLogin(
+    public ResponseEntity<StudentDTO> studentLogin(
             @RequestBody Map<String, String> credentials) {
         String login = credentials.get("login");
         String password = credentials.get("password");
 
         Map<String, Object> response = authService.authenticateStudent(login, password);
-        return ResponseEntity.ok(response);
+        StudentDTO studentDTO = new StudentDTO((UUID) response.get("student_id"), (String) response.get("role"));
+        return ResponseEntity.ok(studentDTO);
     }
 }
