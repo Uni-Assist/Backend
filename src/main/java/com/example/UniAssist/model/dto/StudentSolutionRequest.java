685d2c95724b54970ea7b73770f3227d9153658e
Очнев Артём <a.ochniev@mail.ru>
2025-05-20 14:21:37 +0700

    Add request fields validation

--- src/main/java/com/example/UniAssist/model/dto/StudentSolutionRequest.java
+++ src/main/java/com/example/UniAssist/model/dto/StudentSolutionRequest.java
@@ -1,13 +1,29 @@
 package com.example.UniAssist.model.dto;
 
 import com.example.UniAssist.type.SolutionType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public class StudentSolutionRequest {
+
+    @NotNull
     private UUID taskId;
+
+    @NotBlank
     private String body;
+
+    @NotNull
     private SolutionType type;
+
+    public StudentSolutionRequest() {}
+
+    public StudentSolutionRequest(UUID taskId, String body, SolutionType type) {
+        this.taskId = taskId;
+        this.body = body;
+        this.type = type;
+    }
 
     public UUID getTaskId() {
         return taskId;
--- src/main/java/com/example/UniAssist/model/dto/UpdateMarkRequest.java
+++ src/main/java/com/example/UniAssist/model/dto/UpdateMarkRequest.java
@@ -1,9 +1,19 @@
 package com.example.UniAssist.model.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+
 import java.util.UUID;
 
 public class UpdateMarkRequest {
+
+    @NotNull
     private UUID solutionId;
+
+    @NotNull
+    @PositiveOrZero
+    @Max(100)
     private Integer mark;
 
     public UpdateMarkRequest() {}
